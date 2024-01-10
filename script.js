9b85e05dab7d2009d60b62240f7c9aa374505bf9
Jaedson Macedo <jaedsonbarbosamacedo@gmail.com>
2024-01-10 13:03:40 -0300

    design e funcionalidades de status concluídos!

--- script.js
+++ script.js
@@ -65,21 +65,53 @@ formCvc.addEventListener("keyup", () => {
 const alerta = document.querySelector(".alert")
 const alertaCvc = document.querySelector(".alertCvc")
 const alertaY = document.querySelector(".alertY")
+const alertaNum = document.querySelector("#alertaNum")
+const alertaName = document.querySelector("#alertNome")
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
     const mes = frm.mes.value
     const ano = frm.year.value
     const numSegurança = frm.formCVC.value
+    const numCard = frm.cardNumber.value
+    const nome = frm.formNome.value
     if (mes.length == 0) {
         frm.mes.style.border = "solid 1px red"
-        alerta.innerHTML = "Can't be black"
+        alerta.innerHTML = "Can't be blank"
+    } else {
+        frm.mes.style.border = ""
+        alerta.innerHTML = "&nbsp;"
     }
-    if(numSegurança.length == 0){
+
+    if (numSegurança.length == 0) {
         frm.formCVC.style.border = "solid 1px red"
-        alertaCvc.innerHTML = "Can't be black"
+        alertaCvc.innerHTML = "Can't be blank"
+    } else {
+        frm.formCVC.style.border = ""
+        alertaCvc.innerHTML = "&nbsp;"
     }
-    if(ano.length == 0){
+
+
+    if (ano.length == 0) {
         frm.year.style.border = "solid 1px red"
-        alertaY.innerHTML = "Can't be black"
+        alertaY.innerHTML = "Can't be blank"
+    } else {
+        frm.year.style.border = ""
+        alertaY.innerHTML = "&nbsp;"
+    }
+
+    if (numCard.length == 0) {
+        frm.cardNumber.style.border = "solid 1px red"
+        alertaNum.innerHTML = "Wrong format, number only"
+    } else {
+        frm.cardNumber.style.border = ""
+        alertaNum.innerHTML = "&nbsp;"
+    }
+
+    if (nome.length == 0) {
+        frm.formNome.style.border = "solid 1px red"
+        alertaName.innerHTML = "Can't be blank"
+    } else {
+        frm.formNome.style.border = ""
+        alertaName.innerHTML = "&nbsp;"
     }
 })
